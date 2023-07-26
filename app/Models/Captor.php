af386dbf0e04270ab42b0f321475196e7eca8534
paulbretonpro <paulobreton29800@gmail.com>
2023-07-26 17:39:30 +0700

    feat(date): send date with info necessary for timezone

--- app/Models/Captor.php
+++ app/Models/Captor.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +17,11 @@ class Captor extends Model
         'consumption',
         'datetime'
     ];
+
+    protected function datetime(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => new Carbon($value),
+        );
+    }
 }
--- app/Models/Weather.php
+++ app/Models/Weather.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,4 +22,12 @@ class Weather extends Model
         'wind_speed',
         'datetime'
     ];
+
+
+    protected function datetime(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => new Carbon($value),
+        );
+    }
 }
