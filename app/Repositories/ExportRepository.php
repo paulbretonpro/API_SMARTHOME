cfbabe2ef24515ecd603c979d2691f1c5807ad7c
paulbretonpro <paul.breton.dev@gmail.com>
2023-08-28 16:57:44 +0700

    fix columns and consumption condition

--- app/Repositories/ExportRepository.php
+++ app/Repositories/ExportRepository.php
@@ -12,7 +12,8 @@ class ExportRepository
         $data = DB::table('weather')
             ->join('sensor', 'sensor.datetime', '=', 'weather.datetime')
             ->join('captor', 'captor.datetime', '=', 'weather.datetime')
-            ->select('weather.datetime',  'sensor.temperature as temperature_in', 'weather.temperature as temperature_out', 'sensor.humidity as humidity_in', 'weather.humidity as humidity_out', 'weather.wind_speed', 'weather.feels_like', 'captor.consumption')
+            ->where('captor.consumption', '>=', 100)
+            ->select('sensor.temperature as temperature_in', 'weather.temperature as temperature_out', 'sensor.humidity as humidity_in', 'weather.humidity as humidity_out', 'weather.wind_speed', 'weather.feels_like', 'captor.consumption')
             ->orderBy('weather.datetime', 'asc')
             ->get();
 
