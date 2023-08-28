77248f57487a8fb08ed67f3d08ba566c945456c0
paulbretonpro <paulobreton29800@gmail.com>
2023-08-28 16:51:36 +0700

    fix orderby export

--- app/Repositories/ExportRepository.php
+++ app/Repositories/ExportRepository.php
@@ -13,7 +13,7 @@ class ExportRepository
             ->join('sensor', 'sensor.datetime', '=', 'weather.datetime')
             ->join('captor', 'captor.datetime', '=', 'weather.datetime')
             ->select('weather.datetime',  'sensor.temperature as temperature_in', 'weather.temperature as temperature_out', 'sensor.humidity as humidity_in', 'weather.humidity as humidity_out', 'weather.wind_speed', 'weather.feels_like', 'captor.consumption')
-            ->orderBy('weather.datetime', 'asc')
+            ->orderBy('weather.datetime', 'desc')
             ->get();
 
         return $data;
